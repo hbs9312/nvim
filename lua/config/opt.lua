1c7b4008f7011222851534d4214c218dff8d24fa
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2025-04-04 10:15:24 +0900

    add plugin: neo-tree

--- lua/config/opt.lua
+++ lua/config/opt.lua
@@ -1,2 +1,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+-- 들여쓰기
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    -- { "3rd/image.nvim", opts= {}},
+    {
+      "s1n7ax/nvim-window-picker",
+      version = "2.*",
+      config = function()
+        require("window-picker").setup({
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            bo = {
+              filetype = {"neo-tree", "neo-tree-popup", "notify" },
+              buftype = { "terminal", "quickfix" },
+            },
+          },
+        })
+      end,
+    },
+  },
+  lazy = false,
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+      open_files_using_relative_paths = false,
+      sort_case_insensitive = false, -- used when sorting files and directories in the tree
+      sort_function = nil, -- use a custom function for sorting files and directories in the tree
+    })
+  end,
+  opts = {
+  },
+}
