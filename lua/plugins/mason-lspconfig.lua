2bbc9f26206d769531b864d318a88c675b95a62d
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2025-04-04 13:34:17 +0900

    setup lsp

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,19 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "ts_ls" },
+    })
+
+    require("mason-lspconfig").setup_handlers({
+      function(server)
+        require("lspconfig")[server].setup({})
+      end,
+    })
+  end,
+}
