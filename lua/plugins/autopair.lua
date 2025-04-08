79159314acece27650f2649a3f57a1ec0ef9a175
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2025-04-08 13:35:27 +0900

    setup plugin windwp/nvim-ts-autotag

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -1,6 +1,18 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  opts = {},
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    osopts = {},
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "html", "xml", "javascript", "typescript",
+      "javascriptreact", "typescriptreact", "tsx"
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
 
